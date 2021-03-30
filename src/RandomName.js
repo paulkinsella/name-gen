cb90fc038f7c1bc2e782b641eaa3915b4b584cd6
paulkinsella <kince23@gmail.com>
2021-03-30 21:42:25 +0100

    Update 4 (#5)

--- src/RandomName.js
+++ src/RandomName.js
@@ -15,17 +15,24 @@ const RandomName = (props) => {
   };
 
   return (
-    <> <div className="buttonContainer">
-      <button className="bigButton" onClick={getRandomName}>Get Random Male Name</button>
-    </div>
-      <div className="details">
-        <div className="name">{random.name}</div>
-        <div className="fact">{random.fact}< br />
-          <Opinion random={random} />
+    <>
+      <div className="buttonContainer">
+        <button
+          className="bigButton"
+          onClick={getRandomName}>Get Random Male Name
+        </button>
+      </div>
+      <div className="cardContainer">
+        <div className={random.name ? "card" : "noCard"}>
+          <div className="details">
+            <div className="name">{random.name}</div>
+            <div className="fact">{random.fact}< br />
+              <Opinion random={random} />
+            </div>
+          </div>
         </div>
-      </div></>
-
-
+      </div>
+    </>
   );
 };
 
